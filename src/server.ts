d49ec308a442e1125a7f0bb767930ffe2183840a
Joel <agboolajoel123@gmail.com>
2025-11-15 23:55:38 +0100

    Remove commented-out product fetching route

--- src/server.ts
+++ src/server.ts
@@ -5,22 +5,6 @@ import app from "./app"
 
 dotenv.config()
 
-// app.get('/products', async (req: Request, res: Response) => {
-//   try {
-//     const products = await pool.query('SELECT * FROM products');
-//     res.status(200).json({
-//         result: products.rows.length,
-//         status: 'Success',
-//         data:{
-//             products: products.rows
-//         }
-//     })
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Server error' });
-//   }
-// });
-
 const port = Number(process.env.PORT)
 
 app.listen(port, () => {
