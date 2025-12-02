b941fe60b20b46e81c173fcf77fd739dc9962186
Joel <agboolajoel123@gmail.com>
2025-12-02 17:21:46 +0100

    chore(server): add unhandledRejection/uncaughtException handlers and connectDB initialization

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import {Request, Response, NextFunction} from "express"
 import dotenv from "dotenv"
+import { connectDB, disconnectDB } from "./config/database";
 
 import app from "./app"
 
@@ -7,6 +8,30 @@ dotenv.config()
 
 const port = Number(process.env.PORT)
 
-app.listen(port, () => {
+
+connectDB()
+const server = app.listen(port, () => {
     console.log(`App listening on port: ${port}... `)
 })
+
+// Handle Asynchronous Errors (e.g., rejected promises)
+process.on('unhandledRejection', (err: unknown) => {
+  console.log('💥 UNHANDLED REJECTION! Shutting down...');
+  if (err instanceof Error) {
+    console.log(err.name, err.message);
+  } else {
+    console.log(err);
+  }
+  server.close( async() => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+// Handle Synchronous Errors
+process.on('uncaughtException', async (err) => {
+  console.log('💥 UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  await disconnectDB()
+  process.exit(1);
+});
