3aa5d5431a409eabad4eba9f84b3c40a89c810b9
Joel <agboolajoel123@gmail.com>
2025-12-07 10:24:13 +0100

    feat(schema): add User Zod schema

---
+++ src/Schema/userSchema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+    name: z.string()
+            .min(2, { message: 'Name must be at least 2 characters long'})
+            .trim(),
+    email: z.string()
+            .email({ message: 'Invalid email address'})
+            .trim()
+            .toLowerCase(),
+    password: z.string()
+                .min(8, { message: 'Password must be at least 8 characters long'})
+                .trim(),
+    passwordConfirm: z.string(),
+    roles: z.enum(['USER', 'ADMIN'])
+            .default('USER'),
+    phoneNumber: z.string()
+                .min(10, { message: 'Phone number must be at least 11 characterslong'})
+                .max(15, { message: 'Phone number must be at least 15 characters long'})
+                .optional()
+                .transform((val) => val ? val.trim() : val),
+     profileImage: z.string()
+                .url({ message: 'Invalid URL format for profile image'})
+                .optional()
+}).refine((data) => data.password === data.passwordConfirm, {
+        message: "Passwords do not match",
+        path: ["passwordConfirm"],
+});
+
+
